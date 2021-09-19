0ade15580455b1dab97e553096f26d028b6a7386
Venom <46383815+123Venom@users.noreply.github.com>
2021-09-19 18:04:49 -0400

    v1.5.2
    fixed encoding issue opening changelog

--- script.module.fenomscrapers/lib/fenomscrapers/help/help.py
+++ script.module.fenomscrapers/lib/fenomscrapers/help/help.py
@@ -5,13 +5,16 @@
 
 from fenomscrapers.modules.control import addonPath, addonVersion, joinPath
 from fenomscrapers.windows.textviewer import TextViewerXML
+from fenomscrapers.modules import py_tools
 
+if py_tools.isPY2:
+	from io import open #py2 open() does not support encoding param
 
 def get(file):
 	fenomscrapers_path = addonPath()
 	fenomscrapers_version = addonVersion()
 	helpFile = joinPath(fenomscrapers_path, 'lib', 'fenomscrapers', 'help', file + '.txt')
-	r = open(helpFile)
+	r = open(helpFile, 'r', encoding='utf-8', errors='ignore')
 	text = r.read()
 	r.close()
 	heading = '[B]FenomScrapers -  v%s - %s[/B]' % (fenomscrapers_version, file)
--- script.module.fenomscrapers/lib/fenomscrapers/modules/changelog.py
+++ script.module.fenomscrapers/lib/fenomscrapers/modules/changelog.py
@@ -5,13 +5,16 @@
 
 from fenomscrapers.modules.control import addonPath, addonVersion, joinPath
 from fenomscrapers.windows.textviewer import TextViewerXML
+from fenomscrapers.modules import py_tools
 
+if py_tools.isPY2:
+	from io import open #py2 open() does not support encoding param
 
 def get():
 	fenomscrapers_path = addonPath()
 	fenomscrapers_version = addonVersion()
 	changelogfile = joinPath(fenomscrapers_path, 'changelog.txt')
-	r = open(changelogfile)
+	r = open(changelogfile, 'r', encoding='utf-8', errors='ignore')
 	text = r.read()
 	r.close()
 	heading = '[B]FenomScrapers -  v%s - ChangeLog[/B]' % fenomscrapers_version
